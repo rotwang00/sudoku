401c2c90174a4a1e62a39fc0adbc40d7b1aeb137
Paul Thurmond <paulthurmond@gmail.com>
2024-01-25 20:03:38 -0600

    Create new empty board

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("connected");
+const createBoard = function () {
+  let newBoard = [];
+  for (let i = 1; i <= 81; i++) {
+    let cell = {
+      cellNumber: i,
+      row: 1,
+      column: 2,
+      zone: 4,
+      isSolved: false,
+      values: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    };
+    newBoard.push(cell);
+  }
+  return newBoard;
+};
+
+let board = createBoard();
+console.dir(board);
